099abb729fd1971eeea5042e362ed14b439d6902
elvis.aghaulor <aghaulorelvis@gmail.com>
2026-07-09 01:52:14 +0100

    rename frontend apps

--- client-portal/src/app/app.ts
+++ client-portal/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('customer_banking_frontend');
+  protected readonly title = signal('client-portal');
 }
--- staff-portal/src/app/app.ts
+++ staff-portal/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('corebanking_frontend');
+  protected readonly title = signal('staff-portal');
 }
